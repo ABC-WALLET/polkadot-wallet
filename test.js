bf7469742137fd911d9a776a95d3db55e680780d
mmulika <mulikamike@gmail.com>
2023-03-24 10:16:44 +0300

    transfer

--- test.js
+++ test.js
@@ -30,5 +30,24 @@ const getBalance = async()=>{
     console.log(balance)
 }
 
+const  Transfer = async(req,res)=>{
+    const api = await ApiPromise.create({ provider });
+    const tx = api.tx.balances.transfer(bob.address, 1000000000000);
+    const { nonce } = await api.query.system.account(alice.address);
+    const { signature } = await tx.signAsync(alice, { nonce });
+    const txHash = await tx.send(async ({ events = [], status }) => {
+      if (status.isInBlock) {
+        console.log(`Transaction included at block hash ${status.asInBlock}`);
+        console.log(`Events:`);
+        console.log(events);
+      } else if (status.isFinalized) {
+        console.log(`Transaction finalized at block hash ${status.asFinalized}`);
+      }
+    });
+    
+
+}
+
 CreateAccount()
 getBalance()
+Transfer()
