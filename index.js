57e6984381a6a90ed7312d95f18f60bd91a87156
mmulika <mulikamike@gmail.com>
2023-03-30 15:34:42 +0300

    balance

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 
 const connect = async () => {
-    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+    const wsProvider = new WsProvider('wss://test.azero.dev');
     const api = new ApiPromise({ provider: wsProvider });
     return api.isReady;
 };
@@ -18,7 +18,7 @@ connect().then((api) => {
 const generateAccount = async()=>{
     const { mnemonicGenerate } = require('@polkadot/util-crypto');
     const { Keyring } = require('@polkadot/keyring');
-    const keyring = new Keyring({ type: 'ethereum' });
+    const keyring = new Keyring({ type: 'sr25519' });
     const mnemonic = mnemonicGenerate();
     const account =   keyring.addFromMnemonic(mnemonic);
     console.log(`Address: ${account.address}`);
@@ -35,37 +35,37 @@ const generateAccount = async()=>{
 
 // }
 
-const checkBalance = async (address) => {
-    const provider = new WsProvider('wss://rpc.polkadot.io');
-    const api = await ApiPromise.create({ provider });
-    const { data: { free: balance } } = await api.query.system.account(address);
-    console.log(`Account balance: ${balance}`);
-  }
+// const checkBalance = async (address) => {
+//     const provider = new WsProvider('wss://rpc.polkadot.io');
+//     const api = await ApiPromise.create({ provider });
+//     const { data: { free: balance } } = await api.query.system.account(address);
+//     console.log(`Account balance: ${balance}`);
+//   }
 
 
 
-  //transfer function
+//   //transfer function
 
-const { ApiPromise, Keyring, WsProvider } = require('@polkadot/api');
+// // const { ApiPromise, Keyring, WsProvider } = require('@polkadot/api');
 
-const transfer = async (senderMnemonic, recipientAddress, amount) => {
-  // Connect to the Polkadot network using a WebSocket provider
-  const provider = new WsProvider('wss://rpc.polkadot.io');
-  const api = await ApiPromise.create({ provider });
+// const transfer = async (senderMnemonic, recipientAddress, amount) => {
+//   // Connect to the Polkadot network using a WebSocket provider
+//   const provider = new WsProvider('wss://rpc.polkadot.io');
+//   const api = await ApiPromise.create({ provider });
 
-  // Create a keyring instance and add the sender account from the provided mnemonic
-  const keyring = new Keyring({ type: 'sr25519' });
-  const sender = keyring.addFromMnemonic(senderMnemonic);
+//   // Create a keyring instance and add the sender account from the provided mnemonic
+//   const keyring = new Keyring({ type: 'sr25519' });
+//   const sender = keyring.addFromMnemonic(senderMnemonic);
 
-  // Send the transfer transaction using the Polkadot.js API
-  const txHash = await api.tx.balances.transfer(recipientAddress, amount).signAndSend(sender);
+//   // Send the transfer transaction using the Polkadot.js API
+//   const txHash = await api.tx.balances.transfer(recipientAddress, amount).signAndSend(sender);
 
-  console.log(`Transfer sent with hash ${txHash}`);
-};
+//   console.log(`Transfer sent with hash ${txHash}`);
+// };
 
 
-checkBalance('0xAD5c976acA555b1C5Ed7c801e1e5f708070AA61d')
+// checkBalance('0xAD5c976acA555b1C5Ed7c801e1e5f708070AA61d')
 
-generateAccount()
+// generateAccount()
 
-transfer()
+// transfer()
