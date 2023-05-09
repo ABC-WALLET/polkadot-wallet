a1c38b45fdbafe95d2dcd70822d97e38adfdc052
mmulika <mulikamike@gmail.com>
2023-05-09 23:17:26 +0300

    auth

--- reg.js
+++ reg.js
@@ -1,10 +1,19 @@
 const addMember  = async(regno,name,email){
 
     const email = email.search(email)
-    console.log(email)
+
 
     return email
 
 }
 
 addMember(1000,mike,mulik@MediaList.com)
+
+
+
+const addOuth = async(member){
+
+    const  registermember = await.add(
+        
+    )
+}
