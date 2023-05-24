fa122918b78b72d026d1e12072d3143730328d36
mmulika <mulikamike@gmail.com>
2023-05-24 20:53:43 +0300

    class

--- test.ts
+++ test.ts
@@ -9,3 +9,21 @@ const creatAccount =async (name:string,age:number) => {
 const getName:string = creatAccount(mulika,2)
 
 console.log(getName)
+
+
+class Person{
+    type  person{
+        name:string,
+        id:number
+
+    }
+
+    const createUser = (person)=>{
+        const username = person.name;
+        const id = person.id;
+
+        return username 
+
+    
+    }
+}
